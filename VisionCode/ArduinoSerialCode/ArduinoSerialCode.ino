f751a4c6675069a2991b03af5fc6e75c3c72995f
SachinKonan <sachinkonan480@gmail.com>
2017-02-10 19:06:58 -0700

    added files

---
+++ VisionCode/ArduinoSerialCode/ArduinoSerialCode.ino
@@ -0,0 +1,12 @@
+// Open a serial connection and flash LED when input is received
+
+void setup(){
+  // Open serial connection.
+  Serial.begin(9600);
+  
+}
+
+void loop(){ 
+  Serial.println(analogRead(A0) * 5/10.0);
+} 
+
