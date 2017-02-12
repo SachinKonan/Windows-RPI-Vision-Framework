8b576a9dc718866e6db485af61225fd31a35df66
SachinKonan <sachinkonan480@gmail.com>
2017-02-12 15:15:40 -0700

    added files

--- Combined/main.py
+++ Combined/main.py
@@ -1,6 +1,13 @@
-import testGearVid, testVisionServer
-import multiprocessing
+import testGearVid, visionServer
+from multiprocessing import Process
+import sys
 
 if __name__ == '__main__':
-    testGearVid.realmain()
-    testVisionServer.realmain()
+
+
+    p1 = Process(target=visionServer.realmain())
+    p1.start()
+    p2 = Process(target=testGearVid.realmain())
+    p2.start()
+
+    
