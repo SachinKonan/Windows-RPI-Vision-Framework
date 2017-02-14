fa6906f52e9bfd3f8d2104bebf392fa1167b2ee3
SachinKonan <sachinkonan480@gmail.com>
2017-02-13 22:48:53 -0700

    Added Multi-Processing

--- Combined/main.py
+++ Combined/main.py
@@ -4,7 +4,13 @@ import time
 
 if __name__ == '__main__':
 
-    p1 = Process(target= testVisionServer.realmain())
+    p1 = Process(target= testVisionServer.realmain)
     p1.start()
-
     print('blah')
+
+    p2 = Process(target=testGearVid.realmain)
+    p2.start()
+    print('hello')
+
+    p1.join()
+    p2.join()
