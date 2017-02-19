730afa80c7d2ab9b11364e3275b103bac6a43e2d
SachinKonan <sachinkonan480@gmail.com>
2017-02-19 09:00:54 -0700

    added threads

--- Combined/main.py
+++ Combined/main.py
@@ -1,14 +1,14 @@
 import testGearVid,testVisionServer
-from multiprocessing import Barrier, Lock, Process
+from threading import Thread
 import time
 
 if __name__ == '__main__':
 
-    p1 = Process(target= testVisionServer.realmain)
+    p1 = Thread(target= testGearVid.realmain,args=())
     p1.start()
     print('blah')
 
-    p2 = Process(target=testGearVid.realmain)
+    p2 = Thread(target= testVisionServer.realmain,args=())
     p2.start()
     print('hello')
 
