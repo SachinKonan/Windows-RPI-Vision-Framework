5d414f94282ebc5a98686d597fb18a9874c73678
SachinKonan <sachinkonan480@gmail.com>
2017-03-20 10:22:02 -0700

    changed code

--- Combined/visionServer.py
+++ Combined/visionServer.py
@@ -44,19 +44,14 @@ def contourArea(contours):
 
 
 def widthDistanceCalc(x):
-<<<<<<< HEAD
-	return -0.0003 * math.pow(x, 3) + 0.0881 * x * x - 10.336 * x + 553.9
-		
-=======
     return -0.0003 * math.pow(x, 3) + 0.0881 * x * x - 10.336 * x + 553.9
 
 
->>>>>>> 59b29378850fe3c7348ead6f138e155dfb58b537
 class InetChecker:
     def __init__(self):
         self.inet = False
-        self.command = 'sudo ping -c 1 192.168.1.1'
-        #self.command = 'ping -n 1 192.168.1.1'
+        #self.command = 'sudo ping -c 1 192.168.1.1'
+        self.command = 'ping -n 1 192.168.1.1'
         self.stopped = False
 
     def start(self):
@@ -68,8 +63,8 @@ class InetChecker:
             if self.stopped:
                 return
 
-            response = Popen(self.command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT).stdout.read().decode()
-            #response = Popen(self.command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT,close_fds=True).stdout.read().decode()
+            #response = Popen(self.command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT).stdout.read().decode()
+            response = Popen(self.command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT,close_fds=True).stdout.read().decode()
 
             if ('Network is unreachable' in response):
                 self.inet = False
@@ -103,7 +98,7 @@ class SendThread:
                 self.sock.close()
                 return
 
-            if(not self.steady):
+            if (not self.steady):
                 self.sock.sendto(self.message.encode(), (self.UDP_IP, self.PORT))
             else:
                 pass
@@ -139,7 +134,7 @@ class ReceiveThread:
                 self.sock.close()
                 return
 
-            if(not self.steady):
+            if (not self.steady):
                 data, addr = self.sock.recvfrom(self.BUFF_SIZE)
                 if (data.decode() != ''):
                     self.message = data.decode()
@@ -260,131 +255,6 @@ def realmain():
     lower_green = (55, 140, 110)
     upper_green = (90, 256, 256)
 
-<<<<<<< HEAD
-class dumServeForever:
-	def __init__(self, ip = ''):
-		self.server = ThreadedHTTPServer((ip, 5810), CamHandler)
-		self.stopped = False
-		
-	def start(self):
-		Thread(target=self.update, args=()).start()
-		
-	def update(self):
-		while True:
-			if self.stopped:
-				return
-			self.server.serve_forever()
-
-	def stop(self):
-		self.stopped = True
-
-def realmain():
-	global frame
-	global inet
-
-	lower_green = (55, 140, 110)
-	upper_green = (90, 256, 256)
-
-	UDP_SEND_PORT = 5800
-	UDP_COMP_PORT = 5801
-
-	UDP_IP = ''
-	UDP_RIO = '10.54.65.79'
-	
-	#UDP_RIO = '192.168.43.157'
-	
-	font = cv2.FONT_HERSHEY_SIMPLEX
-	
-
-	send = SendThread(url=UDP_RIO, port= UDP_SEND_PORT)
-	send.start()
-
-	receive = ReceiveThread(url=UDP_IP, port=UDP_COMP_PORT)
-	receive.start()
-
-	ip = ''
-	cap = WebcamVideoStream(src=0).start()
-	os.system('v4l2-ctl --set-ctrl brightness=80')
-
-	secondcap = WebcamVideoStream(src=1).start()
-
-	inet = False
-	
-	internet = InetChecker().start()
-
-	target = dumServeForever()
-	
-	print("starting server ")
-
-	i = 0
-	while True:
-		inet = internet.getInet()
-		if inet:
-			img = cap.read()
-			img1 = secondcap.read()
-			try:
-				t = imutils.resize(img, width=640, height=480)
-			except AttributeError:
-				raise Exception('Connect Cam 1')
-				break
-			
-			try:
-				tcam2 = imutils.resize(img1, width=640, height=480)
-			except AttributeError:
-				raise Exception('Connect Cam 2')
-				break
-			
-			img2 = cv2.GaussianBlur(t, (5, 5), 0)
-			hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
-			# construct a mask for the color "green", then perform
-			# a series of dilations and erosions to remove any small
-			# blobs left in the mask
-			mask = cv2.inRange(hsv, lower_green, upper_green)
-			edged = cv2.Canny(mask, 35, 125)
-
-			# find contours in the mask and initialize the current
-			# (x, y) center of the ball
-			im2, cnts, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-
-			if (len(cnts) >= 1):
-				area, place = contourArea(cnts)
-
-				if (area >= 100):
-					maxc = cnts[place]
-
-					rect = cv2.minAreaRect(maxc)
-					box = cv2.boxPoints(rect)
-					box = np.int0(box)
-					cv2.drawContours(t, [box], 0, (0, 0, 255), 2)
-
-					M = cv2.moments(maxc)
-					cx = int(M['m10'] / M['m00'])  # Center of MASS Coordinates
-					cy = int(M['m01'] / M['m00'])
-					rect = cv2.minAreaRect(maxc)
-					height = rect[1][0]
-					width = rect[1][1]
-
-					widthreal = max(width, height)
-					heightreal = min(width, height)
-					distance = widthDistanceCalc(widthreal)
-
-					cv2.putText(t, '%s in. ' % (round(distance, 2)), (10, 400), font, 0.5, (0, 0, 255), 1)
-
-					send.changeMessage('Y ' + str(cx) + ' ' + str(cy) + ' ' + "{0:.2f}".format(
-						heightreal) + ' ' + "{0:.2f}".format(widthreal))
-			else:
-				send.changeMessage('N')
-
-
-			message = receive.getMessage()
-
-			if (message == '2'):
-				frame = tcam2
-			elif (message == '1'):
-				frame = t
-			elif (message == ''):
-				frame = tcam2
-=======
     UDP_SEND_PORT = 5800
     UDP_COMP_PORT = 5801
 
@@ -494,21 +364,6 @@ def realmain():
         target.join()
 
         sys.exit()
->>>>>>> 59b29378850fe3c7348ead6f138e155dfb58b537
 
-			if (i == 0):
-				dumServeForever.start()
-			i += 1
-		inet = True
-
-	else:
-		pass
-	
-	dumServeForever.stop()
-	send.stop()
-	receive.stop()
-	cap.stop()
-	secondcap.stop()
-	
 if __name__ == '__main__':
     realmain()
